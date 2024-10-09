9134024649c95a37f347f2dc31e5e886a9a17ed3
raphaellpro <826.xxxxx@gmail.com>
2024-10-08 22:27:31 -0300

    Working with state

--- Day 031/100-days-react/src/App.jsx
+++ Day 031/100-days-react/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-import MyComponent from './components/my-components'
+// import MyComponent from './components/my-components'
+import CondicionalComponent from './components/condicional-component.jsx';
 
 function App() {
 
   return (
     <div>
-      <MyComponent/>
+
+      <CondicionalComponent/>
+      {/* <MyComponent
+        title="Título via propriedades(props)"
+        description="Descrição via propriedades(props)"
+      /> */}
     </div>
   )
 }
--- Day 031/100-days-react/src/components/my-components.jsx
+++ Day 031/100-days-react/src/components/my-components.jsx
@@ -1,8 +1,8 @@
-function MyComponent() {
+function MyComponent(props) {
     return (
         <div>
-            <h1>Hello World!</h1>
-            <p>This is my first component.</p>
+            <h1>{props.title}</h1>
+            <p>(props.description)</p>
         </div>
     )
 }
